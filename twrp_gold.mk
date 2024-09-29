bff07a519cd128aebc189b5bb850db876d46d715
eraselk <eraselk@proton.me>
2024-09-30 07:35:11 +0800

    Update twrp_gold.mk

--- twrp_gold.mk
+++ twrp_gold.mk
@@ -6,7 +6,7 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+#l
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device specific configs
-$(call inherit-product, device/xiaomi/zircon/device.mk)
+$(call inherit-product, device/xiaomi/gold/device.mk)
 
 # Device identifier
 PRODUCT_DEVICE := gold
