d60fc00b2c80b14e9ca0934bf16f5c65239be29c
eraselk <eraselk@proton.me>
2024-09-29 22:14:58 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_zircon.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_gold.mk
 
-COMMON_LUNCH_CHOICES := twrp_zircon-eng
+COMMON_LUNCH_CHOICES := twrp_gold-eng
